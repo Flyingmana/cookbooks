31c6141a8cfde16597e36774d8e05608b244f205
flyingmana <flyingmana@googlemail.com>
2011-02-10 21:17:33 +0100

    - update php version to 5.3.5

--- php/attributes/default.rb
+++ php/attributes/default.rb
@@ -1,7 +1,7 @@
 default["php"] = {}
-default["php"][:version] = "5.3.4"
-#sha251 hash
-default["php"][:checksum] = "9ff3684ea5273e54b8e6d2324c8e402feaa6958fe05885d00e0a18dec3098d64"
+default["php"][:version] = "5.3.5"
+#sha256 hash
+default["php"][:checksum] = "95474df979efbf2b1a298998fe9954200f59048681817a97eb18415346fb6ca4"
 default["php"][:prefix] = "/usr/local"
 default["php"][:displayerrors] = false
 default["php"][:logerrors] = true
