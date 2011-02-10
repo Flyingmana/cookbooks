929bedf808f31d5357913c5541f800c4e374ddc9
flyingmana <flyingmana@googlemail.com>
2011-02-10 21:10:11 +0100

    # mixed up php and new couchdb recipe

--- php/recipes/install.rb
+++ php/recipes/install.rb
@@ -1,32 +1,50 @@
-include_recipe "couchdb::prepare"
+include_recipe "php::prepare"
 
-remote_file "/tmp/couchdb-#{node["couchdb"][:version]}.tgz" do
-  source "http://www.apache.org/dist/couchdb/#{node["couchdb"][:version]}/apache-couchdb-#{node["couchdb"][:version]}.tar.gz"
-  checksum node["couchdb"][:checksum]
+remote_file "/tmp/php-#{node["php"][:version]}.tgz" do
+  source "http://www.php.net/get/php-#{node["php"][:version]}.tar.gz/from/www.php.net/mirror"
+  checksum node["php"][:checksum]
 end
 
-execute "couchDB: unpack" do
-  command "cd /tmp && tar -xzf couchdb-#{node["couchdb"][:version]}.tgz"
+execute "PHP: unpack" do
+  command "cd /tmp && tar -xzf php-#{node["php"][:version]}.tgz"
 end
 
+
+php_opts = []
+php_opts << "--with-config-file-path=#{node["php"][:prefix]}/etc"
+php_opts << "--with-config-file-scan-dir=#{node["php"][:prefix]}/etc/php"
+php_opts << "--prefix=#{node["php"][:prefix]}"
+php_opts << "--with-pear=#{node["php"][:prefix]}/pear"
+
+php_exts = []
+php_exts << "--with-gd"
+php_exts << "--with-zlib"
+php_exts << "--with-jpeg-dir=/usr/local/lib/"
+php_exts << "--enable-exif"
+php_exts << "--enable-mbstring"
+php_exts << "--enable-pcntl"
+php_exts << "--enable-shmop"
+php_exts << "--with-openssl"
+php_exts << "--with-mysql=mysqlnd"
+php_exts << "--with-mysqli=mysqlnd"
+php_exts << "--with-pdo-mysql=mysqlnd"
+#php_exts << "--with-pgsql"
+php_exts << "--with-curl"
+php_exts << "--with-tidy"
+php_exts << "--enable-sockets"
   
-execute "couchDB: ./configure" do
-  cwd "/tmp/couchdb-#{node["couchdb"][:version]}"
+execute "PHP: ./configure" do
+
+  cwd "/tmp/php-#{node["php"][:version]}"
   environment "HOME" => "/root"
-  command "./configure"
+
+  command "./configure #{php_opts.join(' ')} #{php_exts.join(' ')}"
+
 end
 
 
-execute "couchDB: make, make install" do
-  cwd "/tmp/couchdb-#{node["couchdb"][:version]}"
+execute "PHP: make, make install" do
+  cwd "/tmp/php-#{node["php"][:version]}"
   environment "HOME" => "/root"
   command "make && make install"
-end
-
-user "couchdb" do
-  comment "CouchDB Administrato"
-  gid "couchdb"
-  home "/usr/local/var/lib/couchdb"
-  shell "/bin/bash"
-  system true
 end
--- php/recipes/prepare.rb
+++ php/recipes/prepare.rb
@@ -1,15 +1,16 @@
-execute "couchDB: apt-get build-dep" do
-  command "apt-get build-dep couchdb"
-end
-
-
-
-package "libicu-dev"
-package "libcurl4-gnutls-dev"
+package "libevent-dev"
+package "openssl"
+package "libssl-dev"
+package "libcurl4-openssl-dev"
+package "libtidy-dev"
+package "build-essential"
+package "libxml2-dev"
+package "bison"
+package "re2c"
 package "libtool"
+package "help2man"
+package "autoconf"
+package "automake"
 
-#package "xulrunner-dev"
-
-#test if a js lib is available
-#sudo apt-get -s install libmozjs
-package "libmozjs0d"
+package "libjpeg8-dev"
+package "libpng12-dev"
