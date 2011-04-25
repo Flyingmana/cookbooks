95cc4e54390bc30039852a30874ab651cbbdeb56
flyingmana <flyingmana@googlemail.com>
2011-04-25 22:44:31 +0200

    add helper cookbook # we have some tasks, which are not covered by chef # apt-get update in this case

---
+++ helper/metadata.rb
@@ -0,0 +1,6 @@
+maintainer        "Daniel Fahlke aka Flyingmana"
+maintainer_email  "flyingmana@googlemail.com"
+description       "There are some Cases which are not covered by standard chef tools"
+version           "0.1.0"
+recipe "helper::package_update", "update package definitions like from apt-get the ´update´"
+supports 'ubuntu'
