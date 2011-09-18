1bef657d409404d216a7083f58562f2bca48974b
flyingmana <flyingmana@googlemail.com>
2011-09-19 00:14:21 +0200

    add basic erlang cookbook

---
+++ erlang/metadata.rb
@@ -0,0 +1,6 @@
+maintainer			"Daniel Fahlke aka Flyingmana"
+maintainer_email	"flyingmana@googlemail.com"
+description 		"install erlang from apt"
+version 				"0.1.0"
+recipe "erlang::install", "Install"
+supports 'ubuntu'
---
+++ erlang/recipes/install.rb
@@ -0,0 +1,7 @@
+
+package "erlang"
+package "erlang-nox"
+package "erlang-src"
+package "erlang-manpages"
+package "erlang-mode"
+package "erlang-dev"
