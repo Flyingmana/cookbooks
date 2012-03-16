49503d3d609d43dd841b60dd0e4c82ac3bc4c596
flyingmana <flyingmana@googlemail.com>
2012-03-16 10:15:27 +0100

    update version of yaws

--- yaws/attributes/default.rb
+++ yaws/attributes/default.rb
@@ -1,6 +1,6 @@
 default["yaws"] = {}
-default["yaws"][:version] = "1.91"
+default["yaws"][:version] = "1.92"
 #sha256 hash
-default["yaws"][:checksum] = "bc72d7b23122afd8f8f8de5c2e793aa6189ac9801f41b56a11a7b49a8409479c"
+default["yaws"][:checksum] = "cde83ca55d8e1b10a89094e1cfdc3e78d91ba9b1036cb5ea4e1994f5922c2357"
 default["yaws"][:user] = "www-data"
 default["yaws"][:group] = "www-data"
