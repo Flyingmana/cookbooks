a9c5148a58a5b60d88edf5218d85552b30e47835
Daniel Fahlke <daniel@digitalmanufaktur.com>
2012-04-23 04:14:40 +0200

    update used git-cola

--- git-cola/attributes/default.rb
+++ git-cola/attributes/default.rb
@@ -1,6 +1,6 @@
 default["git-cola"] = {}
-default["git-cola"][:version] = "1.4.3"
-default["git-cola"][:subfolder] = "davvid-git-cola-83fa8e2"
+default["git-cola"][:version] = "1.7.6"
+default["git-cola"][:subfolder] = "davvid-git-cola-ac28407"
 #sha251 hash
-default["git-cola"][:checksum] = "4ecfb2dbbe641b61aa74aeb78ec3410a35420a3b6c5d3597fc0db88db40b35cb"
+default["git-cola"][:checksum] = "006ea5c0c692cc1a1710d3bab0f301a4117a7e25f6f514876ba8ef361f7f4536"
 default["git-cola"][:prefix] = "/usr/local"
