f2ed5d336a3d5695c7b24cc9b41bcc1b67393148
Flyingmana <flyingmana@googlemail.com>
2014-03-27 14:11:28 +0100

    add some new dependencies

--- workplace1/recipes/packages.rb
+++ workplace1/recipes/packages.rb
@@ -2,10 +2,17 @@ include_recipe "helper::package_update"
 
 package "iotop"
 package "atop"
+package "git"
+package "varnish"
 
 
 remote_file "/usr/bin/n98-magerun.phar" do
   source "https://raw.github.com/netz98/n98-magerun/master/n98-magerun.phar"
+  mode 0755
+end
+
+remote_file "/usr/bin/composer.phar" do
+  source "https://getcomposer.org/composer.phar"
   mode 0755
 end
 
