504a49ce51ddcf597788c0708d186194729773d0
Flyingmana <flyingmana@googlemail.com>
2014-04-15 12:56:15 +0200

    some changes

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-sudo apt-get install ruby ruby-dev libopenssl-ruby rdoc ri irb build-essential wget ssl-cert curl
+#apt-get update
+#apt-get install sudo
+
+sudo apt-get install ruby ruby-dev libopenssl-ruby rdoc ri irb build-essential wget ssl-cert curl git
 
 cd /tmp
-curl -O http://production.cf.rubygems.org/rubygems/rubygems-1.8.10.tgz
-tar zxf rubygems-1.8.10.tgz
-cd rubygems-1.8.10
+curl -O http://production.cf.rubygems.org/rubygems/rubygems-2.2.2.tgz
+tar zxf rubygems-2.2.2.tgz
+cd rubygems-2.2.2
 sudo ruby setup.rb --no-format-executable
 
 sudo gem install chef --no-ri --no-rdoc
